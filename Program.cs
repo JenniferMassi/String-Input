b2bd5f81bb7e8ca72ac312c6b499dc7d42b7217d
Jennifer Massi <jennifer.massi@mscoding.org>
2023-01-07 14:45:50 -0800

    Input

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("What is your name?");
+            string what = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + what + " Where do you live?");
+            string location = "";
+            location = Console.ReadLine();
+
+            Console.WriteLine("Wow! That's GREAT" + location + " sounds like a fun place to live! What car do you drive?");
+            string car = "";
+            car = Console.ReadLine();
+
+            Console.WriteLine("I love those cars!");
+
+        }
+    }
+}
